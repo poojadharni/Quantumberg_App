eeba64f0b0f3642402145259867d2634fce989a1
poojadharni <poojarajuhe@gmail.com>
2025-05-21 09:09:55 +0000

    Initial Commit

---
+++ Quantumberg/src/router.js
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import { session } from './data/session'
+import { userResource } from '@/data/user'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('@/pages/Home.vue'),
+  }
+]
+
+let router = createRouter({
+  history: createWebHistory('/Quantumberg'),
+  routes,
+})
+
+// router.beforeEach(async (to, from, next) => {
+//   let isLoggedIn = session.isLoggedIn
+//   try {
+//     await userResource.promise
+//   } catch (error) {
+//     isLoggedIn = false
+//   }
+
+//   if (to.name === 'Login' && isLoggedIn) {
+//     next({ name: 'Home' })
+//   } else if (to.name !== 'Login' && !isLoggedIn) {
+//     next({ name: 'Login' })
+//   } else {
+//     next()
+//   }
+// })
+
+export default router
